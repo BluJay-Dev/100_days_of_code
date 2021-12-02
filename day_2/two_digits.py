7359e0d4415d8613b17d120f5a619d838d9c6121
jayelms <jay.elms@mirrorweb.com>
2021-12-02 14:23:49 +0000

    digits

---
+++ day_2/two_digits.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+first = int(two_digit_number[0])
+second = int(two_digit_number[1])
+result = first + second
+print(result)
